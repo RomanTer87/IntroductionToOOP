50f7ee2a44b801de529bcca74ea550d64fb56286
1 <1@DESKTOP-NU9I6RS>
2023-06-19 21:26:29 +0800

    Homework OOP done

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -22,7 +22,21 @@ public:
 	{
 		this->y = y;
 	}
+	double distance(Point other)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
+
 };
+double distance(Point A, Point B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	return sqrt(x_distance * x_distance + y_distance * y_distance);
+}
 
 //#define STRUCT_POINT
 
@@ -45,5 +59,16 @@ void main()
 	Point A;
 	A.set_x(2);
 	A.set_y(3);
-	cout << A.get_x() << "\t" << A.get_y() << endl;
+	cout << "A:\t" << A.get_x() << "\t" << A.get_y() << endl;
+
+	Point B;
+	B.set_x(7);
+	B.set_y(8);
+	cout << "B:\t" << B.get_x() << "\t" << B.get_y() << endl;
+
+	cout << "Расстояние от точки 'A' до точки 'B':\t" << A.distance(B) << endl;
+	cout << "Расстояние от точки 'B' до точки 'A':\t" << B.distance(A) << endl;
+
+	cout << "Расстояние между точками А и В: \t" << distance(A, B) << endl;
+	cout << "Расстояние между точками А и В: \t" << distance(B, A) << endl;
 }
