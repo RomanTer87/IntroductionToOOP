413599a70076096206efdb5f46d55dd884f109d2
1 <1@DESKTOP-NU9I6RS>
2023-07-17 21:20:36 +0800

    In progress matrix

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -5,74 +5,40 @@ class Matrix
 {
 	int rows;
 	int cols;
-	double* matrix;
+	double* array;
 public:
-	int rows()const
+	//	Get-Methods
+	int get_rows() const
 	{
 		return rows;
 	}
-	int get_cols()const
+	int get_cols() const
 	{
 		return cols;
 	}
-	double* get_matrix()
+	double** get_array()const
 	{
-		return matrix;
+		return array;
 	}
-	void set_rows(int rows)
+	//	Constructors
+	Matrix()
 	{
-		this->rows = rows;
+		this->rows = 1;
+		this->cols = 1;
+		this->array = new double* [rows] {};
+		for (int i = 0; i < rows; i++) array[i] = new double [cols] {};
+		cout << "DefaultConstructor:\t" << this << endl;
 	}
-	void set_cols(int cols)
+	Matrix(int cols)
 	{
+		this->rows = 1;
 		this->cols = cols;
+		this->array = new double* [rows] {};
+		for (int i = 0; i < rows; i++) array[i] = new double[cols] {};
+
 	}
 
-	// constructors:
-	Matrix(int rows, int cols, double matrix)
-	{
-		this->rows = rows;
-		this->cols = cols;
-		this->matrix = matrix*[rows][cols];
-		cout << "DefaultConstructor" << this << endl;
-	}
-	Matrix(int rows = 0, int cols = 0)
-	{
-		this->cols = cols;
-		this->rows = rows;
-		this->matrix = matrix*[rows][cols];
-		cout << "Constructor:\t" << this << endl;
-	}
-	Matrix(const Matrix& other)
-	{
-		this->cols = other.cols;
-		this->rows = other.rows;
-		cout << "CopyConstructor:\t" << this << endl;
-	}
-	~Matrix()
-	{
-		cout << "Destructor:\t" << this << endl;
-	}
-	//	Operators:
-	Matrix& operator=(const Matrix& other)
-	{
-		this->cols = other.cols;
-		this->rows = other.rows;
-		cout << "CopyAssignment:\t" << this << endl;
-	}
-	// Methods:
-	/*void print()const
-	{
-		this->matrix = matrix;
-		for (int i = 0; i < rows; i++)
-		{
-			for (int j = 0; j < cols; j++)
-			{
-				cout << matrix[i][j] << "\t";
-			}
-			cout << endl;
-		}
-	}*/
+
 };
 
 void main()
